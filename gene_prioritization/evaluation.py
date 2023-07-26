e61eb68562005097a72f9b38afbeb4ba118edb09
Junyoung Kim <62444215+CptAswadu@users.noreply.github.com>
2023-07-27 04:55:55 +0900

    Update evaluation.py
    add contents of functions

--- gene_prioritization/evaluation.py
+++ gene_prioritization/evaluation.py
@@ -1,7 +1,17 @@
+import os
+import requests
+import re
+
 def evaluate_meaningfulness(gpt_response):
   '''
   Try to grep a GENE SYMBOL (using regular expression)
   [A-Z0-9]+
+  '''
+  gene_regex = r'\b[A-Z0-9]{2,}\b'
+  gene_list = re.findall(gene_regex, text)
+
+  ''' 
+  need to add is the gene meaningfull, ex) GeneA, GeneB, Gene1, Gene2 are not
   '''
   pass
 
@@ -9,13 +19,39 @@ def evaluate_accuracy(gpt_response, true_gene_symbol):
   '''
   Match greped GENE SYMBOL with true_gene_symbol
   '''
-  pass
+  gene_regex = r'\b[A-Z0-9]{2,}\b'
+  gene_list = re.findall(gene_regex, text)
+  
+  if true_gene_symbol in gene_list:
+        return 1
+    else:
+        return 0
 
-def evaluate_fulfillment(gpt_response):
+  '''
+  need to add how to import true_gene_symbole
+  '''
+  pass
+  
+
+def evaluate_fulfillment(gpt_response, top_n_list):
   '''
   Try to match predifined JSON (using regular expression)
   \{"gene_list"\s*:\s*"(?:[A-Z0-9]+(?:\s*,\s*[A-Z0-9]+)*)"\}
   '''
+  gene_regex = r'\b[A-Z0-9]{2,}\b'
+  gene_list = re.findall(gene_regex, text)
+
+  if len(gene_list) > 0:
+    if str(len(gene_list)) == top_n_list:
+      return 1
+    else:
+      return0
+  else:
+    if "X":
+      return 1
+    else:
+      retiun 0
+      
   pass
 
 def get_gpt_response(sample,top_n, prompt, gpt_version, input_type, iteration):
